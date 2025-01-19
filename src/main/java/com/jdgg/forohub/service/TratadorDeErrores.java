dc0f24cb9a7f256fe765946ab5570c60bd4fb4cc
JoseDGG <davidgarciag.343@gmail.com>
2025-01-19 16:14:03 -0500

    Se agrega clase tratador de errores para valid

---
+++ src/main/java/com/jdgg/forohub/service/TratadorDeErrores.java
@@ -0,0 +1,25 @@
+package com.jdgg.forohub.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    //Esta excepcion ocurre cuando una validacion de un argumento anotado @Valid falla.
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    //Se utiliza para representar un error de validacion con dos campos.
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
